e7c5c6c43cc9af5232ae384ef05de6e032a84d51
Joseph Pulk <joseph.pulk@netapp.com>
2025-01-25 20:03:33 -0500

    UPDATED zsh config and PowerShell Profile

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,24 +1,27 @@
 Import-Module -Name Microsoft.PowerShell.Management, Microsoft.PowerShell.Security, Microsoft.PowerShell.Utility, NetApp.ONTAP, Terminal-Icons -Verbose:$false
+
 oh-my-posh init pwsh --config "$( ([System.IO.FileInfo]$PROFILE).directory.FullName )/amro.omp.json" | Invoke-Expression
 
 try
 {
     Import-Module VMware.PowerCLI -ErrorAction SilentlyContinue
 }
-catch {}
+catch
+{
+}
 
 $options = Get-PSReadLineOption
 $options.EditMode = 'VI'
 $options.ParameterColor = $PSStyle.Foreground.FromRGB('#8DEECC')
 $options.VariableColor = $PSStyle.Foreground.FromRGB('#39CDF5')
 
-Set-PSReadLineKeyHandler -Key Tab -Function Complete
+Set-PSReadLineKeyHandler -Chord Tab -Function Complete
 
 . "$( ([System.IO.FileInfo]$PROFILE).Directory.FullName )/aliases.ps1"
 . "$( ([System.IO.FileInfo]$PROFILE).Directory.FullName )/argument_completers.ps1"
 . "$( ([System.IO.FileInfo]$PROFILE).Directory.FullName )/defaultParameters.ps1"
 . "$( ([System.IO.FileInfo]$PROFILE).Directory.FullName )/general.ps1"
-. "$( ([System.IO.FileInfo]$PROFILE).Directory.FullName )/bitwarden.ps1"
+#. "$( ([System.IO.FileInfo]$PROFILE).Directory.FullName )/bitwarden.ps1"
 
 if ($psEditor.Workspace.Path)
 {
--- .zshrc.d/exports.zsh
+++ .zshrc.d/exports.zsh
@@ -1,16 +1,19 @@
+# Set up fzf key bindings and fuzzy completion
+source <(fzf --zsh)
+
 #Use fzf-tmux
 export FZF_TMUX=1
 
 # When you click ctrl+o then it will open the file in vscode
 export FZF_DEFAULT_OPTS="
 --layout=reverse
---bind='ctrl-o:execute(code {})+abort'
+--bind='ctrl-e:execute(nvim {})+abort'
 --bind 'ctrl-a:select-all'"
 
 # The command to use instead of find.
-export FZF_DEFAULT_COMMAND='ag --hidden --norecurse --ignore .git -g ""'
+export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow --glob "!.git/*"'
 
-# Set the default editor to helix
+# Set the default editor to NVIM!!
 export EDITOR='nvim'
 
 # Set the default viewer for midnight commander
@@ -18,3 +21,29 @@ export VIEWER='bat'
 
 # Location of my zettlekasten notes
 export ZK_NOTEBOOK_DIR='/Users/jpulk/git/Personal/dailyZK'
+
+
+export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
+  --highlight-line \
+  --info=inline-right \
+  --ansi \
+  --layout=reverse \
+  --border=none
+  --color=bg+:#283457 \
+  --color=bg:#16161e \
+  --color=border:#27a1b9 \
+  --color=fg:#c0caf5 \
+  --color=gutter:#16161e \
+  --color=header:#ff9e64 \
+  --color=hl+:#2ac3de \
+  --color=hl:#2ac3de \
+  --color=info:#545c7e \
+  --color=marker:#ff007c \
+  --color=pointer:#ff007c \
+  --color=prompt:#2ac3de \
+  --color=query:#c0caf5:regular \
+  --color=scrollbar:#27a1b9 \
+  --color=separator:#ff9e64 \
+  --color=spinner:#ff007c \
+"
+
