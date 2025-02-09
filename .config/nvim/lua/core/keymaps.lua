4d0a29ea85828a8f12db719cd1670d3fdfac1e19
Joseph Pulk <joseph.pulk@netapp.com>
2025-02-08 22:10:35 -0500

    Removed unused keymaps and fixed line wrap keymap

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -1,24 +1,8 @@
----Test if a file exists on the file system
----@param filePath any
----@return boolean
-local function file_exists(filePath)
-    local f = io.open(filePath, "r")
-    if f ~= nil then
-        io.close(f)
-        return true
-    else
-        return false
-    end
-end
-
-local function open_file(filePath)
-    vim.cmd(string.format("edit %s", filePath))
-end
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 local k = vim.keymap
-local zkPath = vim.fn.expand("$HOME/git/Personal/dailyZK")
+
 --Open ntrw
 k.set("n", "<leader>pv", vim.cmd.Ex)
 k.set("n", "-", vim.cmd.Ex)
@@ -44,32 +28,8 @@ k.set("n", "<leader>ac", "<cmd>tabnew<CR>", { desc = "Create a new tab" })
 k.set("n", "<leader>an", "<cmd>tabNext<CR>", { desc = "Move to next tab" })
 k.set("n", "<leader>ap", "<cmd>tabprevious<CR>", { desc = "Move to previous tab" })
 
--- Open notes.
-vim.api.nvim_set_keymap(
-    "n",
-    "<leader>zo",
-    "<Cmd>ZkNotes { sort = { 'modified' } }<CR>",
-    { noremap = true, silent = false }
-)
-
-k.set("n", "<leader>t", function()
-    local monthNotePath = string.format("%s/journal/todo/%s", zkPath, os.date("%Y-%m.md"))
-    local lastMonthPath =
-        string.format("%s/journal/todo/%s", zkPath, os.date("%Y-%m.md", os.time() - 31 * 24 * 60 * 60))
-
-    if file_exists(monthNotePath) then
-        open_file(monthNotePath)
-    elseif file_exists(lastMonthPath) then
-        os.execute(string.format("cp %1 %2", monthNotePath, lastMonthPath))
-        open_file(lastMonthPath)
-    else
-        print("Previous File missing: " .. lastMonthPath)
-    end
-end, { desc = "Open [T]odays Zk" })
-
-k.set("n", "<leader>st", function()
-    vim.cmd(string.format("TodoTelescope cwd=%1", zkPath))
-end, { desc = "Find [S]earch [T]odo Items in Zk" })
+-- Open Zettlekasten Note
+k.set("n", "<leader>zo", "<Cmd>ZkNotes { sort = { 'modified' } }<CR>", { desc = "[Z]k [O]pen a note" })
 
 -- Todo
 k.set("n", "]t", function()
@@ -87,14 +47,13 @@ k.set({ "n", "v" }, "<leader>f", function()
         async = false,
         timeout_ms = 500,
     })
-end, { desc = "Format file or range (in visual mode)" })
+end, { desc = "[F]ormat file or range (in visual mode)" })
 
-k.set("n", "<M-z>", function()
-    if vim.opt.wrap then
-        vim.opt.wrap = false
-    else
-        vim.opt.wrap = true
-    end
+-- Toggle line wrap using vscode style keymap
+k.set("n", "<A-z>", function()
+    vim.notify(string.format("%s wrap", vim.o.wrap and "Enabling" or "Disabling"), vim.log.levels.INFO)
+
+    vim.o.wrap = not vim.o.wrap
 end, { desc = "Enable word wrap" })
 
 --LSP
